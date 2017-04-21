889c168bb1425de239b6eed3aa54fae447664e5a
JUNCHENG WANG <wangjunc@10-150-44-231.dhcp-m.deakin.edu.au>
2017-04-21 16:46:56 +1000

    Initial Commit

---
+++ github_demo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github_demo1
+//
+//  Created by JUNCHENG WANG on 21/04/2017.
+//  Copyright © 2017 JUNCHENG WANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
